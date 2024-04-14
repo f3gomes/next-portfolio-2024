a07dc187c29f03fdbc14185e892349d9fd4e3bfd
fomes <fgomesdeluna@gmail.com>
2024-04-14 09:12:27 -0300

    feat(know techs): creates know techs section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import HeroSection from "./components/sections/home/hero-section";
+import { KnownTechs } from "./components/sections/home/known-techs";
 
 export default async function Home() {
   return (
     <>
       <HeroSection />
+      <KnownTechs
+        techs={[{ name: "Javascript", iconSvg: "", startDate: "10-10-2021" }]}
+      />
     </>
   );
 }

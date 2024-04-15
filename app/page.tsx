790e41d4830dcf2c357d53f9fb9cb06cdfe9ea20
fomes <fgomesdeluna@gmail.com>
2024-04-15 08:55:03 -0300

    feat(experience section): creates section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
 import HeroSection from "./components/sections/home/hero-section";
 import { HighlightedProjects } from "./components/sections/home/highlighted-projects";
 import { KnownTechs } from "./components/sections/home/known-techs";
+import { WorkExperience } from "./components/sections/home/work-experience";
 
 export default async function Home() {
   const techs = [{ name: "Javascript", iconSvg: "", startDate: "10-10-2021" }];
+  const experiences: any = [
+    {
+      companyLogo: {
+        url: "/images/hero-bg.png",
+      },
+      role: "role",
+      companyName: "companyName",
+      companyUrl: "companyUrl",
+      startDate: "10-10-2021",
+      endDate: "10-10-2022",
+      technologies: [],
+      description: {
+        raw: "",
+      },
+    },
+  ];
   const projects: any = [
     {
       slug: "slug",
@@ -26,6 +43,7 @@ export default async function Home() {
       <HeroSection />
       <KnownTechs techs={techs} />
       <HighlightedProjects projects={projects} />
+      <WorkExperience experiences={experiences} />
     </>
   );
 }

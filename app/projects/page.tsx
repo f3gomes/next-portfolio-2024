dcba9287b5f0a17dd38ddff18299436222307c46
fomes <fgomesdeluna@gmail.com>
2024-04-15 10:20:40 -0300

    feat(projects): create project list and card

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,10 +1,57 @@
 import React from "react";
-import { PageIntroduction } from "../components/sections/projects/project-intro";
+import { PageIntroduction } from "../components/sections/projects/page-intro";
+import { ProjectsList } from "../components/sections/projects/projects-list";
 
 export default function Projects() {
+  const projects: any = [
+    {
+      slug: "slug",
+      thumbnail: {
+        url: "/images/hero-bg.png",
+      },
+      title: "Title",
+      shortDescription: "Short Description",
+      technologies: [],
+      pageThumbnail: { url: "PageThumbnail" },
+      sections: [],
+      description: { text: "Text Description" },
+      liveProjectUrl: "LiveProject",
+      githubUrl: "Github URL",
+    },
+    {
+      slug: "slug",
+      thumbnail: {
+        url: "/images/hero-bg.png",
+      },
+      title: "Title",
+      shortDescription: "Short Description",
+      technologies: [],
+      pageThumbnail: { url: "PageThumbnail" },
+      sections: [],
+      description: { text: "Text Description" },
+      liveProjectUrl: "LiveProject",
+      githubUrl: "Github URL",
+    },
+    {
+      slug: "slug",
+      thumbnail: {
+        url: "/images/hero-bg.png",
+      },
+      title: "Title",
+      shortDescription: "Short Description",
+      technologies: [],
+      pageThumbnail: { url: "PageThumbnail" },
+      sections: [],
+      description: { text: "Text Description" },
+      liveProjectUrl: "LiveProject",
+      githubUrl: "Github URL",
+    },
+  ];
+
   return (
     <>
       <PageIntroduction />
+      <ProjectsList projects={projects} />
     </>
   );
 }

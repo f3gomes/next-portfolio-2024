06f423ee60fe97fa935cdcd60588e91a9210f73f
fomes <fgomesdeluna@gmail.com>
2024-04-15 10:10:36 -0300

    feat(projects): page introduction

---
+++ app/projects/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { PageIntroduction } from "../components/sections/projects/project-intro";
+
+export default function Projects() {
+  return (
+    <>
+      <PageIntroduction />
+    </>
+  );
+}
